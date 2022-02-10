a72c8923928b2dd5c6086afae1cb65b8d0fd5f59
Lewis Wright <lewis@wrightlinedesign.com>
2022-02-10 19:45:20 +0000

    rename variables to match Unreal convention

--- triplex.cpp
+++ triplex.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 
 int main() {
-    std::cout << "You are a secret agent breaking into number 10 to break up a party";
+    std::cout << "You are a secret agent breaking into number 10 to break up a party" << std::endl;
+    std::cout << "Sneaky bastards! They've got code locks on the doors! You're going to need to break the codes..." << std::endl;
+
+    const int CodeA = 4;
+    const int CodeB = 7;
+    const int CodeC = 5;
+
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
     std::cout << std::endl;
-    std::cout << "Sneaky bastards! They've got code locks on the doors! You're going to need to break the codes...";
-
-    const int a = 4;
-    const int b = 7;
-    const int c = 5;
-
-    const int sum = a + b + c;
-    const int product = a * b * c;
-
-    std::cout << std::endl;
-    std::cout << "There are 3 numbers in the code" << std::endl;
-    std::cout << "The codes add up to: " << sum << std::endl;
-    std::cout << product << std::endl;
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ The codes add up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes mutliply to give:" << CodeProduct << std::endl;
 
     return 0;
 }
