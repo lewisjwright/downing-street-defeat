6c0e2c1b46e5c10975182d72608e9a1167d3c483
Lewis Wright <lewis@wrightlinedesign.com>
2022-02-10 01:08:20 +0000

    hello world code and readme

--- triplex.cpp
+++ triplex.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+
 int main() {
+    std::cout << "You are a secret agent breaking into number 10 to break up a party";
+    std::cout << std::endl;
+    std::cout << "Sneaky bastards! They've got code locks on the doors! You're going to need to break the codes...";
+
     return 0;
 }
